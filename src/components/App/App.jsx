11ff1f1e24c27d325d2046f599e64bd230d71913
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-08-28 08:13:51 -0500

    used axios to GET gallery images into app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [galleryItems, setGalleryItems] = useState('')
+
+  useEffect(() => {
+    console.log('useEffect page-load');
+    fetchGalleryImages();
+  }, []);
+
+  const fetchGalleryImages = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    }).then(response => {
+      setGalleryItems(response.data);
+      console.log(response.data); //Testing GET
+    }).catch(error => {
+      console.log(error);
+      alert('Something went wrong!')
+    });
+  }
     return (
       <div className="App">
         <header className="App-header">
